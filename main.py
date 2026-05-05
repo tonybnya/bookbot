61f2c37b42bf79ec07c41db275bc350c81ba167c
tonybnya <tonybnya@gmail.com>
2026-05-05 18:14:56 +0100

    feat: add more textbooks and handle command line arguments

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ Description : Entry point of the CLI
 Author      : @tonybnya
 """
 
+import sys
 from stats import get_book_text, get_num_chars, get_num_words, sort_list_dict
 
 
 def main() -> None:
-    FILE: str = './books/frankenstein.txt'
-    contents: str = get_book_text(FILE)
+    if len(sys.argv) != 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    book_path: str = sys.argv[1]
+    contents: str = get_book_text(book_path)
     chars: dict[str, int] = get_num_chars(contents)
     list_dict: list[dict[str, int | str]] = sort_list_dict(chars)
     print(get_num_words(contents))
