5155a4dd29b37672d150127f03fd087119bed0fb
tonybnya <tonybnya@gmail.com>
2026-05-05 16:19:38 +0100

    feat: count words

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ def get_book_text(filepath: str) -> str:
     return contents
 
 
+def count_words(contents: str) -> str:
+    num_words: int = len(contents.split())
+    return f'Found {num_words} total words'
+
+
 def main() -> None:
     FILE: str = './books/frankenstein.txt'
-    print(get_book_text(FILE))
+    # print(get_book_text(FILE))
+    contents: str = get_book_text(FILE)
+    print(count_words(contents))
 
 
 if __name__ == '__main__':
