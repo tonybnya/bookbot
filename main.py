731730b45cbb601ddce71796f5d5234e2b639fac
tonybnya <tonybnya@gmail.com>
2026-05-05 16:07:50 +0100

    feat: read a given text file

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 """
 Script Name : main.py
-Description : Describe what this script does
+Description : CLI program
 Author      : @tonybnya
 """
 
-print("greetings boots")
+def get_book_text(filepath: str) -> str:
+    with open(filepath) as f:
+        contents = f.read()
+    return contents
+
+
+def main() -> None:
+    FILE: str = './books/frankenstein.txt'
+    print(get_book_text(FILE))
+
+
+if __name__ == '__main__':
+    main()
