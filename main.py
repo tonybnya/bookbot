a60131e24dafa39554fd2d2d3f93f083bcca0468
tonybnya <tonybnya@gmail.com>
2026-05-05 17:32:14 +0100

    feat: count chars

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ Description : Entry point of the CLI
 Author      : @tonybnya
 """
 
-from stats import get_book_text, get_num_words
+from stats import get_book_text, get_num_chars, get_num_words
 
 
 def main() -> None:
     FILE: str = './books/frankenstein.txt'
-    print(get_book_text(FILE))
     contents: str = get_book_text(FILE)
+    chars: dict[str, int] = get_num_chars(contents)
     print(get_num_words(contents))
+    print(chars)
 
 
 if __name__ == '__main__':
--- stats.py
+++ stats.py
@@ -7,10 +7,17 @@ Author      : @tonybnya
 
 def get_book_text(filepath: str) -> str:
     with open(filepath) as f:
-        contents = f.read()
-    return contents
+        text = f.read()
+    return text
 
 
-def get_num_words(contents: str) -> str:
-    num_words: int = len(contents.split())
+def get_num_words(text: str) -> str:
+    num_words: int = len(text.split())
     return f'Found {num_words} total words'
+
+
+def get_num_chars(text: str) -> dict[str, int]:
+    chars: dict[str, int] = {}
+    for c in text:
+        chars[c.lower()] = chars.get(c.lower(), 0) + 1
+    return chars
